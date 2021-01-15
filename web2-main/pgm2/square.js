561dc2cc6b6bf470458746122e7ecbbbb52c81f0
vasavids2906 <74951246+vasavids2906@users.noreply.github.com>
2021-01-15 19:43:07 +0530

    Add files via upload

---
+++ web2-main/pgm2/square.js
@@ -0,0 +1,6 @@
+document.write("<table> <colspan='3'>");
+for (var n=0;n<=10;n++)
+{
+    document.write("<tr><td>"+n+"</td><td>"+n*n+"</td><td>"+n*n*n+"</td></tr>");
+}
+document.write("</table>");
